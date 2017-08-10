6081727e49e919a7607f8f430a71a020d0cfd309
Luke Childs <lukechilds123@gmail.com>
2017-08-10 21:19:56 +0100

    Flesh out basic functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
 'use strict';
 
-const ReactJSDOM = () => {};
+const Window = require('window');
+const ReactDOM = require('react-dom');
+
+const ReactJSDOM = {
+	version: ReactDOM.version,
+	render: component => {
+		const window = new Window();
+		const document = window.document;
+
+		const origGlobals = {
+			window: global.window,
+			document: global.document
+		};
+		global.window = window;
+		global.document = document;
+
+		const container = document.createElement('div');
+		ReactDOM.render(component, container);
+
+		Object.keys(origGlobals).forEach(prop => {
+			global[prop] = origGlobals[prop];
+		});
+
+		return container.children.length > 1 ? container.children : container.children[0];
+	}
+
+};
 
 module.exports = ReactJSDOM;
--- test/react-jsdom.js
+++ test/react-jsdom.js
@@ -1,6 +1,15 @@
 import test from 'ava';
+import React from 'react';
 import ReactJSDOM from 'this';
 
-test('ReactJSDOM is a function', t => {
-	t.is(typeof ReactJSDOM, 'function');
+test('ReactJSDOM is a object', t => {
+	t.is(typeof ReactJSDOM, 'object');
+});
+
+test('ReactJSDOM renders a React Component', t => {
+	const component = ReactJSDOM.render(
+		React.createElement('div', {}, 'hi')
+	);
+	t.is(component.nodeName, 'DIV');
+	t.is(component.textContent, 'hi');
 });
